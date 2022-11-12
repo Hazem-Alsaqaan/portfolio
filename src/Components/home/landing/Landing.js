e552a576fa7b747ade08a16fd03c344b23774306
Hazem-Alsaqaan <hazem.alsaqaan@gmail.com>
2022-11-12 23:48:54 +0200

    update CV url

--- src/Components/home/landing/Landing.js
+++ src/Components/home/landing/Landing.js
@@ -14,14 +14,17 @@ const Landing = ()=>{
                                     <h4 className="hey">Hello, I'm</h4>
                                     <h1 className="name">Hazem Alsaqaan</h1>
                                 </div>
-                                <p>I am a Frontend engineer with an eye for design, 
-                                    I specialize in building design systems and applications in 
-                                    React, edit and building large projects. I like to contribute
-                                    with designing pages to better reflect the design, especially 
-                                    I excited build and edit large projects.
+                                <p>
+                                I am a solution-driven  MERN stack developer familiar with a wide range 
+                                of programming utilities and languages, knowledgeable of front-end and back-end
+                                development requirements, handles any part of process with easy, I have proven 
+                                experience in developing consumer-focused websites , meeting highest standard 
+                                for web design , user experience, best practices and speed, designed and developed
+                                web applications across multiple APIs , third party integrations and data bases.
+                                I am a collaborative team player with excellent technical abilities.
                                 </p>
                             </div>
-                            <a href="https://drive.google.com/file/d/1OBtrr0iMiE0sze2BVOb8OPvHTXGhSptw/view?usp=sharing" target="post">
+                            <a href="https://drive.google.com/file/d/1Di34UhvUgVcymtyPjBdGtpFukRJ2KHO3/view?usp=sharing" target="post">
                                 <button className="btn-download">Download CV <FontAwesomeIcon icon={faArrowDown}/></button>
                                 </a>
                         </div>
