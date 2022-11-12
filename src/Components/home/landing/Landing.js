510acfebf1c01842edb782f8256e01c6fac014e7
Hazem-Alsaqaan <hazem.alsaqaan@gmail.com>
2022-11-13 00:45:13 +0200

    update my bio

--- src/Components/home/landing/Landing.js
+++ src/Components/home/landing/Landing.js
@@ -14,14 +14,14 @@ const Landing = ()=>{
                                     <h4 className="hey">Hello, I'm</h4>
                                     <h1 className="name">Hazem Alsaqaan</h1>
                                 </div>
-                                <p>
-                                I am a solution-driven  MERN stack developer familiar with a wide range 
-                                of programming utilities and languages, knowledgeable of front-end and back-end
-                                development requirements, handles any part of process with easy, I have proven 
-                                experience in developing consumer-focused websites , meeting highest standard 
-                                for web design , user experience, best practices and speed, designed and developed
-                                web applications across multiple APIs , third party integrations and data bases.
-                                I am a collaborative team player with excellent technical abilities.
+                                <p>                         
+                                **I am a MERN stack developer** with an eye for design, I specialize
+                                in building systems and applications focused websites using HTML, CSS,
+                                Javascript, React, node, express, mongodb ,knowledgeable of front-end 
+                                and back-end development requirements, I can handle any part of process 
+                                with easy,  I like to contribute with designing pages to better reflect 
+                                the design ,edit and building large projects
+
                                 </p>
                             </div>
                             <a href="https://drive.google.com/file/d/1Di34UhvUgVcymtyPjBdGtpFukRJ2KHO3/view?usp=sharing" target="post">
