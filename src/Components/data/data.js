ccd16ee5ac825fe0c27274120b6fae514dd076c2
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2023-06-13 17:12:14 +0300

    add sayf project

--- src/Components/data/data.js
+++ src/Components/data/data.js
@@ -2,48 +2,55 @@
 const projects = [
     {
         id: 1,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/agenda_backend",
+        codeUrl: "https://github.com/Hazem-Alsaqaan/sayf",
+        webSitUrl: "https://super-reaction.surge.sh",
+        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1686579619/sayf/screencapture-super-reaction-surge-sh-sayf_zwfjwi.png",
+        projectTitle: "sayf"
+    },
+    {
+        id: 2,
+        codeUrl: "https://github.com/Hazem-Alsaqaan/Judicial-agenda",
         webSitUrl: "http://public-agenda.surge.sh/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583336/projects/agenda-surge-sh_tnznwm.png",
         projectTitle: "Agenda"
     },
     {
-        id: 2,
+        id: 3,
         codeUrl: "https://github.com/Hazem-Alsaqaan/facebook",
         webSitUrl: "http://scared-school.surge.sh/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583346/projects/facebook_lbh0xx.png",
         projectTitle: "Social App"
     },
     {
-        id: 3,
+        id: 4,
         codeUrl: "https://github.com/Hazem-Alsaqaan/ecommerce",
         webSitUrl: "https://hazem-alsaqaan.github.io/ecommerce/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840870/projects/ecommerce_tcez0c.png",
         projectTitle: "Ecommerce"
     },
     {
-        id: 4,
+        id: 5,
         codeUrl: "https://github.com/Hazem-Alsaqaan/NOTFLIX",
         webSitUrl: "https://hazem-alsaqaan.github.io/NOTFLIX/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841066/projects/NOTFLIX-_gbertm.png",
         projectTitle: "Movies & Series"
     },
     {
-        id: 5,
+        id: 6,
         codeUrl: "https://github.com/Hazem-Alsaqaan/bookstore",
         webSitUrl: " https://hazem-alsaqaan.github.io/bookstore/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840919/projects/bookstore_khpfzg.png",
         projectTitle: "BookStore"
     },
     {
-        id: 6,
+        id: 7,
         codeUrl: "https://github.com/Hazem-Alsaqaan/third-test-",
         webSitUrl: "https://hazem-alsaqaan.github.io/third-test-/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841084/projects/third_vnkaxw.png",
         projectTitle: "Gallery"
     },
     {
-        id: 7,
+        id: 8,
         codeUrl: "https://codepen.io/hazem-alsaqaan/pen/OJvPzZO",
         webSitUrl: "https://codepen.io/hazem-alsaqaan/full/OJvPzZO",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841105/projects/memory-game_nbnejw.png",
@@ -53,11 +60,11 @@ const projects = [
 const skills = [
     {   
         id: 1,
-        title: "HTML-5",
+        title: "HTML",
     },
     {   
         id: 2,
-        title: "CSS-3",
+        title: "CSS",
     },
     {   
         id: 3,
@@ -69,18 +76,26 @@ const skills = [
     },
     {   
         id: 5,
-        title: "BootStrap",
+        title: "Next.js",
     },
     {   
         id: 6,
-        title: "node-js",
+        title: "React Native",
     },
     {   
         id: 7,
-        title: "mongo-db",
+        title: "BootStrap",
     },
     {   
         id: 8,
+        title: "node-js",
+    },
+    {   
+        id: 9,
+        title: "mongo-db",
+    },
+    {   
+        id: 10,
         title: "express",
     },
 ];
--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import {DataContext} from './Components/context/DataContext';
-// import { BrowserRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DataContext>
-    {/* <BrowserRouter> */}
       <App />
-    {/* </BrowserRouter> */}
     </DataContext>
   </React.StrictMode>
 );
