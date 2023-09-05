57e708812d552ea7acbe3173f14c1973a1eab925
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2023-09-05 15:52:22 +0300

    add two projects to my portfolio ( lawyer agenda & egypt house )

--- src/Components/data/data.js
+++ src/Components/data/data.js
@@ -5,52 +5,59 @@ const projects = [
         codeUrl: "https://github.com/Hazem-Alsaqaan/sayf",
         webSitUrl: "https://super-reaction.surge.sh",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1686579619/sayf/screencapture-super-reaction-surge-sh-sayf_zwfjwi.png",
-        projectTitle: "sayf"
+        projectTitle: "Sayf"
     },
     {
         id: 2,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/Judicial-agenda",
-        webSitUrl: "http://public-agenda.surge.sh/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583336/projects/agenda-surge-sh_tnznwm.png",
-        projectTitle: "Agenda"
+        codeUrl: "https://github.com/Hazem-Alsaqaan/agenda_website",
+        webSitUrl: "https://lawyeragenda.surge.sh",
+        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1693917089/judicial_agenda/screencapture-lawyeragenda-surge-sh-2023-09-05-15_28_57_dyqag7.png",
+        projectTitle: "Lawyer Agenda"
     },
     {
         id: 3,
+        codeUrl: "https://github.com/Hazem-Alsaqaan/Aqar_Alex",
+        webSitUrl: "https://egypthouse.surge.sh",
+        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1693917321/judicial_agenda/screencapture-egypthouse-surge-sh-2023-09-05-15_34_50_aizl9n.png",
+        projectTitle: "Egypt House"
+    },
+    {
+        id: 4,
         codeUrl: "https://github.com/Hazem-Alsaqaan/facebook",
         webSitUrl: "http://scared-school.surge.sh/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583346/projects/facebook_lbh0xx.png",
         projectTitle: "Social App"
     },
     {
-        id: 4,
+        id: 5,
         codeUrl: "https://github.com/Hazem-Alsaqaan/ecommerce",
         webSitUrl: "https://hazem-alsaqaan.github.io/ecommerce/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840870/projects/ecommerce_tcez0c.png",
         projectTitle: "Ecommerce"
     },
     {
-        id: 5,
+        id: 6,
         codeUrl: "https://github.com/Hazem-Alsaqaan/NOTFLIX",
         webSitUrl: "https://hazem-alsaqaan.github.io/NOTFLIX/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841066/projects/NOTFLIX-_gbertm.png",
         projectTitle: "Movies & Series"
     },
     {
-        id: 6,
+        id: 7,
         codeUrl: "https://github.com/Hazem-Alsaqaan/bookstore",
         webSitUrl: " https://hazem-alsaqaan.github.io/bookstore/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840919/projects/bookstore_khpfzg.png",
         projectTitle: "BookStore"
     },
     {
-        id: 7,
+        id: 8,
         codeUrl: "https://github.com/Hazem-Alsaqaan/third-test-",
         webSitUrl: "https://hazem-alsaqaan.github.io/third-test-/",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841084/projects/third_vnkaxw.png",
         projectTitle: "Gallery"
     },
     {
-        id: 8,
+        id: 9,
         codeUrl: "https://codepen.io/hazem-alsaqaan/pen/OJvPzZO",
         webSitUrl: "https://codepen.io/hazem-alsaqaan/full/OJvPzZO",
         Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841105/projects/memory-game_nbnejw.png",
@@ -88,14 +95,18 @@ const skills = [
     },
     {   
         id: 8,
-        title: "node-js",
+        title: "tailwind",
     },
     {   
         id: 9,
-        title: "mongo-db",
+        title: "node-js",
     },
     {   
         id: 10,
+        title: "mongo-db",
+    },
+    {   
+        id: 11,
         title: "express",
     },
 ];
