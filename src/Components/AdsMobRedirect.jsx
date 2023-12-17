84b886b9b86cdd0d1748e204d566c41839e267a1
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2023-12-17 21:16:48 +0200

    update routes

--- src/Components/AdsMobRedirect.jsx
+++ src/Components/AdsMobRedirect.jsx
@@ -5,7 +5,7 @@ const AdsMobRedirect = () => {
     return (
         <>
             <div className="App">
-                <p style={{ margin: 8 }}>google.com, pub-9498389929500961, DIRECT, f08c47fec0942fa0</p>
+                <p>google.com, pub-9498389929500961, DIRECT, f08c47fec0942fa0</p>
             </div>
         </>
     )
