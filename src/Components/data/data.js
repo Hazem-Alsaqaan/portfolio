5eea2c6a5effba47f080d97f1da8bf1b51d02ce2
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2024-01-22 13:54:02 +0200

    replace lawyer agenda website with lawyer agenda android application

--- src/Components/data/data.js
+++ src/Components/data/data.js
@@ -1,114 +1,123 @@
-
 const projects = [
-    {
-        id: 1,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/sayf",
-        webSitUrl: "https://super-reaction.surge.sh",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1686579619/sayf/screencapture-super-reaction-surge-sh-sayf_zwfjwi.png",
-        projectTitle: "Sayf"
-    },
-    {
-        id: 2,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/agenda_website",
-        webSitUrl: "https://lawyeragenda.surge.sh",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1693917089/judicial_agenda/screencapture-lawyeragenda-surge-sh-2023-09-05-15_28_57_dyqag7.png",
-        projectTitle: "Lawyer Agenda"
-    },
-    {
-        id: 3,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/Aqar_Alex",
-        webSitUrl: "https://egypthouse.surge.sh",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1693917321/judicial_agenda/screencapture-egypthouse-surge-sh-2023-09-05-15_34_50_aizl9n.png",
-        projectTitle: "Egypt House"
-    },
-    {
-        id: 4,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/facebook",
-        webSitUrl: "http://scared-school.surge.sh/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583346/projects/facebook_lbh0xx.png",
-        projectTitle: "Social App"
-    },
-    {
-        id: 5,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/ecommerce",
-        webSitUrl: "https://hazem-alsaqaan.github.io/ecommerce/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840870/projects/ecommerce_tcez0c.png",
-        projectTitle: "Ecommerce"
-    },
-    {
-        id: 6,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/NOTFLIX",
-        webSitUrl: "https://hazem-alsaqaan.github.io/NOTFLIX/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841066/projects/NOTFLIX-_gbertm.png",
-        projectTitle: "Movies & Series"
-    },
-    {
-        id: 7,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/bookstore",
-        webSitUrl: " https://hazem-alsaqaan.github.io/bookstore/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840919/projects/bookstore_khpfzg.png",
-        projectTitle: "BookStore"
-    },
-    {
-        id: 8,
-        codeUrl: "https://github.com/Hazem-Alsaqaan/third-test-",
-        webSitUrl: "https://hazem-alsaqaan.github.io/third-test-/",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841084/projects/third_vnkaxw.png",
-        projectTitle: "Gallery"
-    },
-    {
-        id: 9,
-        codeUrl: "https://codepen.io/hazem-alsaqaan/pen/OJvPzZO",
-        webSitUrl: "https://codepen.io/hazem-alsaqaan/full/OJvPzZO",
-        Poster: "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841105/projects/memory-game_nbnejw.png",
-        projectTitle: "Memory Game"
-    },
-]
+  {
+    id: 1,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/sayf",
+    webSitUrl: "https://super-reaction.surge.sh",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1686579619/sayf/screencapture-super-reaction-surge-sh-sayf_zwfjwi.png",
+    projectTitle: "Sayf",
+  },
+  {
+    id: 2,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/nativeAgendaApp",
+    webSitUrl:
+      "https://play.google.com/store/apps/details?id=com.hazemalsaqaan.Agenda",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1705924352/Blue_and_White_Simple_IU_UX_Design_App_xnvicz.jpg",
+    projectTitle: "Lawyer Agenda",
+  },
+  {
+    id: 3,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/Aqar_Alex",
+    webSitUrl: "https://egypthouse.surge.sh",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1693917321/judicial_agenda/screencapture-egypthouse-surge-sh-2023-09-05-15_34_50_aizl9n.png",
+    projectTitle: "Egypt House",
+  },
+  {
+    id: 4,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/facebook",
+    webSitUrl: "http://scared-school.surge.sh/",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1678583346/projects/facebook_lbh0xx.png",
+    projectTitle: "Social App",
+  },
+  {
+    id: 5,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/ecommerce",
+    webSitUrl: "https://hazem-alsaqaan.github.io/ecommerce/",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840870/projects/ecommerce_tcez0c.png",
+    projectTitle: "Ecommerce",
+  },
+  {
+    id: 6,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/NOTFLIX",
+    webSitUrl: "https://hazem-alsaqaan.github.io/NOTFLIX/",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841066/projects/NOTFLIX-_gbertm.png",
+    projectTitle: "Movies & Series",
+  },
+  {
+    id: 7,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/bookstore",
+    webSitUrl: " https://hazem-alsaqaan.github.io/bookstore/",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679840919/projects/bookstore_khpfzg.png",
+    projectTitle: "BookStore",
+  },
+  {
+    id: 8,
+    codeUrl: "https://github.com/Hazem-Alsaqaan/third-test-",
+    webSitUrl: "https://hazem-alsaqaan.github.io/third-test-/",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841084/projects/third_vnkaxw.png",
+    projectTitle: "Gallery",
+  },
+  {
+    id: 9,
+    codeUrl: "https://codepen.io/hazem-alsaqaan/pen/OJvPzZO",
+    webSitUrl: "https://codepen.io/hazem-alsaqaan/full/OJvPzZO",
+    Poster:
+      "https://res.cloudinary.com/dkhu7rt8n/image/upload/v1679841105/projects/memory-game_nbnejw.png",
+    projectTitle: "Memory Game",
+  },
+];
 const skills = [
-    {   
-        id: 1,
-        title: "HTML",
-    },
-    {   
-        id: 2,
-        title: "CSS",
-    },
-    {   
-        id: 3,
-        title: "JavaScript",
-    },
-    {   
-        id: 4,
-        title: "React.js",
-    },
-    {   
-        id: 5,
-        title: "Next.js",
-    },
-    {   
-        id: 6,
-        title: "React Native",
-    },
-    {   
-        id: 7,
-        title: "BootStrap",
-    },
-    {   
-        id: 8,
-        title: "tailwind",
-    },
-    {   
-        id: 9,
-        title: "node-js",
-    },
-    {   
-        id: 10,
-        title: "mongo-db",
-    },
-    {   
-        id: 11,
-        title: "express",
-    },
+  {
+    id: 1,
+    title: "HTML",
+  },
+  {
+    id: 2,
+    title: "CSS",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+  },
+  {
+    id: 4,
+    title: "React.js",
+  },
+  {
+    id: 5,
+    title: "Next.js",
+  },
+  {
+    id: 6,
+    title: "React Native",
+  },
+  {
+    id: 7,
+    title: "BootStrap",
+  },
+  {
+    id: 8,
+    title: "tailwind",
+  },
+  {
+    id: 9,
+    title: "node-js",
+  },
+  {
+    id: 10,
+    title: "mongo-db",
+  },
+  {
+    id: 11,
+    title: "express",
+  },
 ];
 
-export  {projects, skills};
+export { projects, skills };
--- src/Components/projects/Projects.js
+++ src/Components/projects/Projects.js
@@ -1,34 +1,40 @@
-import React, { Fragment, useContext} from "react";
+import React, { Fragment, useContext } from "react";
 import "./Projects.css";
-import  {projectsContext}  from "../context/DataContext";
+import { projectsContext } from "../context/DataContext";
 import PagesTitle from "../pages-title/PagesTitle";
 
-const Portfolio = ()=>{
-    const {projects,} = useContext(projectsContext)
-    
-    return(
-        <Fragment>
-                <section id="projects" className="projects">
-                        <PagesTitle title = "portfolio"  paragraph = "collection of websites and applications like e-commerce , movies, bookstrore  and games" />
-                    <div className="portfolio container">
-                        {
-                            projects.map((oneItem)=>{return (
-                                    <div key={oneItem.id} className="box-project">
-                                        
-                                            <h1>{oneItem.projectTitle}</h1>
-                                            <div className="box-bt">
-                                                <div className="box-img">
-                                                    <img src={oneItem.Poster} alt=""/>
-                                                </div>
-                                                <a target="blank" href={oneItem.codeUrl}><button>watch code</button></a>
-                                                <a target="blank" href={oneItem.webSitUrl}><button>visit project</button></a>
-                                            </div>
-                                    </div>
-                            )})
-                        }
-                    </div>
-                </section>
-        </Fragment>
-    )
-}
+const Portfolio = () => {
+  const { projects } = useContext(projectsContext);
+
+  return (
+    <Fragment>
+      <section id="projects" className="projects">
+        <PagesTitle
+          title="portfolio"
+          paragraph="collection of websites and applications like e-commerce , movies, bookstrore  and games"
+        />
+        <div className="portfolio container">
+          {projects.map((oneItem) => {
+            return (
+              <div key={oneItem.id} className="box-project">
+                <h1>{oneItem.projectTitle}</h1>
+                <div className="box-bt">
+                  <div className="box-img">
+                    <img src={oneItem.Poster} alt="" />
+                  </div>
+                  <a target="blank" href={oneItem.codeUrl}>
+                    <button>watch code</button>
+                  </a>
+                  <a target="blank" href={oneItem.webSitUrl}>
+                    <button>visit project</button>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </Fragment>
+  );
+};
 export default Portfolio;
